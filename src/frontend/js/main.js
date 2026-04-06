81bdf9d4f54489648dcfa3c6907d2265be55f512
DiegoACastroQ <254183995+DiegoACastroQ@users.noreply.github.com>
2026-04-06 02:03:08 +0200

    Feat: implement pointer-based navigation and object-driven authentication logic

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,19 +1,58 @@
-let registerBtn = document.querySelector("#registerBtn");
-let loginBtn = document.querySelector("#loginBtn");
-let logo = document.querySelector("#logo");
-let interfaceBtn = document.querySelector("#interfaceBtn");
-let interfaceCont = document.querySelector("#interfaceMainContainer");
-let receptionCont = document.querySelector("#receptionMainContainer");
+const testCont=document.querySelector("#congrats")
+const registerBtn = document.querySelector("#registerBtn");
+const loginBtn = document.querySelector("#loginBtn");
+const logo = document.querySelector("#logo");
+const interfaceBtn = document.querySelector("#interfaceContinue");
+const interfaceCont = document.querySelector("#interfaceMainContainer");
+const receptionCont = document.querySelector("#receptionMainContainer");
+const interfaceGoBack = document.querySelector("#interfaceBack");
+const interfaceInputs = document.querySelector("#interfaceInputs");
+const interfaceText = document.querySelector("#interfaceText");
+const loginInputUser= document.querySelector("#loginUserInput");
+const loginInputPassword=document.querySelector("#loginPasswordInput");
 
-
-function Swap(hidde,appear) {
-    hidde.classList.toggle("hidden") 
-    appear.classList.toggle("hidden") 
+let currentDisplay=receptionCont;
+let userLoginData= {
+    username:"Diego",
+    password:"1234"
+}
+let userLoginInput={
+    username:"",
+    password:""
+}
+function interfaceMode(mode) {
+  interfaceText.textContent=mode;
+  interfaceBtn.textContent=mode;  
+    
+}
+function navigateTo(newDisplay) {
+    currentDisplay.classList.add("hidden") 
+    newDisplay.classList.remove("hidden") 
+    
+    currentDisplay=newDisplay;
+}
+function loginCheck(userInput) {
+   if(userInput.username===userLoginData.username && userInput.password===userLoginData.password){
+   navigateTo(testCont)
+   }
+   
+    
 }
 
 loginBtn.addEventListener("click",()=> {
-Swap(receptionCont,interfaceCont);
+
+interfaceMode("Login")    
+navigateTo(interfaceCont);
+});
+registerBtn.addEventListener("click",()=> {
+interfaceMode("Register")      
+navigateTo(interfaceCont);
+});
+interfaceGoBack.addEventListener("click",()=> {
+navigateTo(receptionCont);
 });
 interfaceBtn.addEventListener("click",()=> {
-Swap(interfaceCont,receptionCont);
+   userLoginInput.username=loginInputUser.value;
+   userLoginInput.password=loginInputPassword.value;
+loginCheck(userLoginInput);
 });
